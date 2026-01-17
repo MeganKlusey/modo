289a0969d0f73ffdf8d495f0eb49c18132d0812b
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-01-17 01:52:51 +0000

    Update site title

--- themes/modo/header.php
+++ themes/modo/header.php
@@ -4,12 +4,12 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Modo</title>
+  <title><?php echo get_bloginfo( 'name' ); ?></title>
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
   <header>
-    <h1>Modo</h1>
+    <h1><?php echo get_bloginfo( 'name' ); ?></h1>
   </header>

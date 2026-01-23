8b451ba867709db833b492d1d052854ea0ce4b7b
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-01-23 19:03:01 +0000

    Hero content positioning progress

--- themes/modo/template-parts/home/hero.php
+++ themes/modo/template-parts/home/hero.php
@@ -1,4 +1,11 @@
 <section class="hero">
-  <img src="<?php echo esc_url(wp_get_attachment_url(23)); ?>" alt="" class="hero__image" />
-  <h1 class="hero__title"><?php the_title(); ?></h1>
+	<img src="<?php echo esc_url(wp_get_attachment_url(23)); ?>" alt="" class="hero__image" />
+	<div class="container">
+		<h1 class="hero__title">Modo<br /> Museum</h1>
+		<div class="hero__description">
+			<p>Step into a world of timeless stories, rare collections, and unforgettable experiences.
+			</p>
+			<p>(Since 2008)</p>
+		</div>
+	</div>
 </section>

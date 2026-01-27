ca13810657d335a1b696e0db64e6b5585f3b55aa
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-01-27 18:53:33 +0000

    Primary menu styling progress

--- themes/modo/header.php
+++ themes/modo/header.php
@@ -2,12 +2,18 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php echo get_bloginfo( 'name' ); ?></title>
-  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-  <?php wp_head(); ?>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo get_bloginfo( 'name' ); ?></title>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-  <header></header>
+	<header>
+		<?php
+			wp_nav_menu(array(
+				"menu" => "Primary Menu"
+			));
+		?>
+	</header>

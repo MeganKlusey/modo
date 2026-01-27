fbd964f2175a6ea22fda00444d2d1afd69dbf80a
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-01-27 16:37:12 +0000

    Position hero main

--- themes/modo/template-parts/home/hero.php
+++ themes/modo/template-parts/home/hero.php
@@ -1,11 +1,13 @@
 <section class="hero">
 	<img src="<?php echo esc_url(wp_get_attachment_url(23)); ?>" alt="" class="hero__image" />
 	<div class="container">
-		<h1 class="hero__title"><span>Modo</span><span>Museum</span></h1>
-		<div class="hero__description">
-			<p>Step into a world of timeless stories, rare collections, and unforgettable experiences.
-			</p>
-			<p>(Since 2008)</p>
+		<div class="hero__main">
+			<h1 class="hero__title"><span>Modo</span><span>Museum</span></h1>
+			<div class="hero__description">
+				<p class="text-sm">Step into a world of timeless stories, rare collections, and unforgettable experiences.
+				</p>
+				<p class="text-sm">(Since 2008)</p>
+			</div>
 		</div>
 	</div>
 </section>

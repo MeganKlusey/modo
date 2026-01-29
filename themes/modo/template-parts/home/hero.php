b746e8789645bbfeafcb08f51ac2c9341ebd2a49
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-01-29 01:00:15 +0000

    Add offsite ctas

--- themes/modo/template-parts/home/hero.php
+++ themes/modo/template-parts/home/hero.php
@@ -1,12 +1,30 @@
 <section class="hero">
 	<img src="<?php echo esc_url(wp_get_attachment_url(23)); ?>" alt="" class="hero__image" />
-	<div class="container">
-		<div class="hero__main">
+	<div class="wrapper">
+		<div class="hero__main--left">
+			<ul>
+				<?php 
+					$offsiteCTAs = array(
+						["label" => "X", "url" => "#"],
+						["label" => "LinkedIn", "url" => "#"],
+						["label" => "Instagram", "url" => "#"],
+						["label" => "hello@megan.com", "url" => "#"],
+					);
+
+					foreach ($offsiteCTAs as $offsiteCTA) {
+				?>
+				<li>
+					<a href="<?= $offsiteCTA["url"] ?>" class="text-xs"><?= $offsiteCTA["label"]; ?></a>
+				</li>
+				<?php } ?>
+			</ul>
+		</div>
+		<div class="hero__main--right">
 			<h1 class="hero__title"><span>Modo</span><span>Museum</span></h1>
 			<div class="hero__description">
 				<p class="text-sm">Step into a world of timeless stories, rare collections, and unforgettable experiences.
 				</p>
-				<p class="text-sm">(Since 2008)</p>
+				<p class="text-sm">(Since 2018)</p>
 			</div>
 		</div>
 	</div>

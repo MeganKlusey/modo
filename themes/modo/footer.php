eb3e717fded51f08b2805a4082709fe8feafbbb8
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-03 12:52:39 +0000

    Footer wrapper

--- themes/modo/footer.php
+++ themes/modo/footer.php
@@ -1,48 +1,50 @@
 <footer>
-	<div class="wrapper">
-		<div class="case-studies">
-			<h2 class="case-studies__title text-xs">Case Studies</h2>
-			<div class="case-studies__right">
-				<p class="case-studies__text text-4xl">Great design isn't just about what you make. It's about who you make it
-					with.</p>
-				<a href="#" class="case-studies__link text-xs">
-					<? get_template_part("src/inc/icons/bullet-point") ?> Get started
-				</a>
+	<div class="wrapper-padding">
+		<div class="wrapper">
+			<div class="case-studies">
+				<h2 class="case-studies__title text-xs">Case Studies</h2>
+				<div class="case-studies__right">
+					<p class="case-studies__text text-4xl">Great design isn't just about what you make. It's about who you make it
+						with.</p>
+					<a href="#" class="case-studies__link text-xs">
+						<? get_template_part("src/inc/icons/bullet-point") ?> Get started
+					</a>
+				</div>
 			</div>
-		</div>
-		<div class="links">
-			<div class="links__left">
-				<nav>
-					<?php
-							wp_nav_menu(array(
-								"menu" => "Primary Menu",
-								"container" => "false",
-								"link_class" => "text-xs"
-							));
-						?>
-				</nav>
-				<ul class="links__offsite">
-					<?php 
-							$offsiteLinks = array(
-								["label" => "X", "url" => "#"],
-								["label" => "LinkedIn", "url" => "#"],
-								["label" => "Instagram", "url" => "#"],
-								["label" => "hello@megan.com", "url" => "#"],
-							);
+			<div class="links">
+				<div class="links__left">
+					<nav>
+						<?php
+									wp_nav_menu(array(
+										"menu" => "Primary Menu",
+										"container" => "false",
+										"link_class" => "text-xs"
+									));
+								?>
+					</nav>
+					<ul class="links__offsite">
+						<?php 
+									$offsiteLinks = array(
+										["label" => "X", "url" => "#"],
+										["label" => "LinkedIn", "url" => "#"],
+										["label" => "Instagram", "url" => "#"],
+										["label" => "hello@megan.com", "url" => "#"],
+									);
 
-							foreach ($offsiteLinks as $offsiteLink) {
-						?>
-					<li>
-						<a href="<?= $offsiteLink["url"] ?>" class="text-xs"><?= $offsiteLink["label"]; ?></a>
-					</li>
-					<?php } ?>
-				</ul>
-			</div>
-			<div class="links__right">
-				<p class="text-xs">LA, California</p>
-				<div>
-					<h3 class="text-xs">Office hours</h3>
-					<p class="text-xs">Monday - Friday 11 AM - 2 PM</p>
+									foreach ($offsiteLinks as $offsiteLink) {
+								?>
+						<li>
+							<a href="<?= $offsiteLink["url"] ?>" class="text-xs"><?= $offsiteLink["label"]; ?></a>
+						</li>
+						<?php } ?>
+					</ul>
+				</div>
+				<div class="links__right">
+					<p class="text-xs">LA, California</p>
+					<div>
+						<h3 class="text-xs">Office hours</h3>
+						<p class="text-xs">Monday - Friday 11 AM - 2 PM</p>
+					</div>
 				</div>
 			</div>
 		</div>

a1e9f57ef90bddb0fe50ac53fcdc903d96bf774c
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-03 00:00:08 +0000

    Amend ctas to links

--- themes/modo/template-parts/home/hero.php
+++ themes/modo/template-parts/home/hero.php
@@ -2,19 +2,19 @@
 	<img src="<?php echo esc_url(wp_get_attachment_url(23)); ?>" alt="" class="hero__image" />
 	<div class="wrapper">
 		<div class="hero__main--left">
-			<ul>
+			<ul class="offsite-links">
 				<?php 
-					$offsiteCTAs = array(
+					$offsiteLinks = array(
 						["label" => "X", "url" => "#"],
 						["label" => "LinkedIn", "url" => "#"],
 						["label" => "Instagram", "url" => "#"],
 						["label" => "hello@megan.com", "url" => "#"],
 					);
 
-					foreach ($offsiteCTAs as $offsiteCTA) {
+					foreach ($offsiteLinks as $offsiteLink) {
 				?>
 				<li>
-					<a href="<?= $offsiteCTA["url"] ?>" class="text-xs"><?= $offsiteCTA["label"]; ?>
+					<a href="<?= $offsiteLink["url"] ?>" class="text-xs"><?= $offsiteLink["label"]; ?>
 						<?php get_template_part('src/inc/icons/arrow'); ?>
 					</a>
 				</li>

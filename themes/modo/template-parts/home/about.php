870c5c3245515d7478f601bc0afd4b896b5c7f73
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-04 11:42:10 +0000

    About progress

--- themes/modo/template-parts/home/about.php
+++ themes/modo/template-parts/home/about.php
@@ -1,6 +1,23 @@
 <section class="about">
-	<h2>About</h2>
-	<p>Founded to preserve and celebrate art, history, and culture, our museum brings together treasures from across
-		centuries and continents. Whether you're passionate about ancient civilizations, fascinated by modern art, or
-		looking for an inspiring day out, our galleries and experiences offer something for everyone.</p>
+	<div class="wrapper">
+		<h2 class="text-xs">About</h2>
+		<p class="text-2xl">Founded to preserve and celebrate art, history, and culture, our museum brings together
+			treasures from across centuries and continents. Whether you're passionate about ancient civilizations, fascinated
+			by modern art, or looking for an inspiring day out, our galleries and experiences offer something for everyone.
+		</p>
+		<?php
+			$images = [
+				["label" => "", "url" => "5"],
+				["label" => "", "url" => "6"],
+				["label" => "", "url" => "7"],
+				["label" => "", "url" => "8"],
+				["label" => "", "url" => "9"],
+				["label" => "", "url" => "10"],
+			];
+
+			foreach($images as $image) {
+		?>
+		<img src="<?= esc_url(wp_get_attachment_url($image["url"])); ?>" alt="" class="" />
+		<?php } ?>
+	</div>
 </section>

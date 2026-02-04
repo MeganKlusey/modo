deffda516d8e074c17ce006a4d721f1f41b86e67
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-04 00:54:12 +0000

    Setup home about template part

--- themes/modo/page-templates/home.php
+++ themes/modo/page-templates/home.php
@@ -4,7 +4,8 @@
 ?>
 
 <main>
-  <?php get_template_part('template-parts/home/hero'); ?>
+	<?php get_template_part('template-parts/home/hero'); ?>
+	<?php get_template_part('template-parts/home/about'); ?>
 </main>
 
 <?php get_footer(); ?>
---
+++ themes/modo/template-parts/home/about.php
@@ -0,0 +1,6 @@
+<section class="about">
+	<h2>About</h2>
+	<p>Founded to preserve and celebrate art, history, and culture, our museum brings together treasures from across
+		centuries and continents. Whether you're passionate about ancient civilizations, fascinated by modern art, or
+		looking for an inspiring day out, our galleries and experiences offer something for everyone.</p>
+</section>

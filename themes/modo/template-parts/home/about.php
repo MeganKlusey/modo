55ecb73c30b9f5de7c5d100d9be87751184ba2eb
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-04 12:36:03 +0000

    Add wrapper padding

--- themes/modo/template-parts/home/about.php
+++ themes/modo/template-parts/home/about.php
@@ -1,27 +1,32 @@
 <section class="about">
-	<div class="wrapper">
-		<div class="about__text">
-			<h2 class="text-xs">About</h2>
-			<p class="text-2xl">Founded to preserve and celebrate art, history, and culture, our museum brings together
-				treasures from across centuries and continents. Whether you're passionate about ancient civilizations,
-				fascinated
-				by modern art, or looking for an inspiring day out, our galleries and experiences offer something for everyone.
-			</p>
-		</div>
-		<?php
-			$images = [
-				["label" => "1", "url" => "5"],
-				["label" => "2", "url" => "6"],
-				["label" => "3", "url" => "7"],
-				["label" => "4", "url" => "8"],
-				["label" => "5", "url" => "9"],
-				["label" => "6", "url" => "10"],
-			];
+	<div class="wrapper-padding">
+		<div class="wrapper">
+			<div class="about__text">
+				<h2 class="text-xs">About</h2>
+				<p class="text-2xl">Founded to preserve and celebrate art, history, and culture, our museum brings together
+					treasures from across centuries and continents. Whether you're passionate about ancient civilizations,
+					fascinated
+					by modern art, or looking for an inspiring day out, our galleries and experiences offer something for
+					everyone.
+				</p>
+			</div>
+			<div class="about__images">
+				<?php
+				$images = [
+					["label" => "1", "url" => "5"],
+					["label" => "2", "url" => "6"],
+					["label" => "3", "url" => "7"],
+					["label" => "4", "url" => "8"],
+					["label" => "5", "url" => "9"],
+					["label" => "6", "url" => "10"],
+				];
 
-			foreach($images as $image) {
-		?>
-		<img src="<?= esc_url(wp_get_attachment_url($image["url"])); ?>" alt=""
-			class="image image-<?= $image["label"]; ?>" />
-		<?php } ?>
+				foreach($images as $image) {
+			?>
+				<img src="<?= esc_url(wp_get_attachment_url($image["url"])); ?>" alt=""
+					class="image image-<?= $image["label"]; ?>" />
+				<?php } ?>
+			</div>
+		</div>
 	</div>
 </section>

1980a2e91d9c7b754545fde922918e71b2a07032
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-04 12:13:10 +0000

    Position about text

--- themes/modo/template-parts/home/about.php
+++ themes/modo/template-parts/home/about.php
@@ -1,10 +1,13 @@
 <section class="about">
 	<div class="wrapper">
-		<h2 class="text-xs">About</h2>
-		<p class="text-2xl">Founded to preserve and celebrate art, history, and culture, our museum brings together
-			treasures from across centuries and continents. Whether you're passionate about ancient civilizations, fascinated
-			by modern art, or looking for an inspiring day out, our galleries and experiences offer something for everyone.
-		</p>
+		<div class="about__text">
+			<h2 class="text-xs">About</h2>
+			<p class="text-2xl">Founded to preserve and celebrate art, history, and culture, our museum brings together
+				treasures from across centuries and continents. Whether you're passionate about ancient civilizations,
+				fascinated
+				by modern art, or looking for an inspiring day out, our galleries and experiences offer something for everyone.
+			</p>
+		</div>
 		<?php
 			$images = [
 				["label" => "", "url" => "5"],

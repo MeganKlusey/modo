00d517993b55c2309574e609fc070809f1b1d403
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-05 01:48:43 +0000

    Added wrapper

--- themes/modo/template-parts/home/exhibitions.php
+++ themes/modo/template-parts/home/exhibitions.php
@@ -1,1 +1,13 @@
-<section class="exhibitions"></section>
+	<section class="exhibitions">
+		<div class="wrapper-padding">
+			<div class="wrapper">
+				<div class="exhibitions__left">
+					<h2 class="text-xs">Gallery exhibitions</h2>
+					<div>
+						<img src="" alt="" class="" />
+					</div>
+				</div>
+				<div class="exhibitions__right"></div>
+			</div>
+		</div>
+	</section>

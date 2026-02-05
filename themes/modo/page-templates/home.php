fef8895a757cf3b5811a74ac8b21319207765585
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-05 01:04:58 +0000

    Setup home exhibitions template part

--- themes/modo/page-templates/home.php
+++ themes/modo/page-templates/home.php
@@ -6,6 +6,7 @@
 <main>
 	<?php get_template_part('template-parts/home/hero'); ?>
 	<?php get_template_part('template-parts/home/about'); ?>
+	<?php get_template_part('template-parts/home/exhibitions'); ?>
 </main>
 
 <?php get_footer(); ?>

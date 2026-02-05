518042f07d28df18c648796becb93845d628d4f6
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-05 19:00:34 +0000

    Set first item on initial load

--- themes/modo/src/js/home/_exhibitions.js
+++ themes/modo/src/js/home/_exhibitions.js
@@ -1,8 +1,12 @@
-const items = document.querySelectorAll(".exhibitions__item");
+document.addEventListener("DOMContentLoaded", function () {
+  const items = document.querySelectorAll(".exhibitions__item");
 
-items.forEach((item) => {
-  item.addEventListener("mouseenter", () => {
-    items.forEach((i) => (i.style.opacity = 0.3));
-    item.style.opacity = 1;
+  items[0].style.opacity = 1;
+
+  items.forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+      items.forEach((i) => (i.style.opacity = 0.3));
+      item.style.opacity = 1;
+    });
   });
 });

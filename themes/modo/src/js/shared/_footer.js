9a3691fd20a4ff4867db8ff3ffc8b7c231a18c3a
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-05 19:00:12 +0000

    DOM content loaded

--- themes/modo/src/js/shared/_footer.js
+++ themes/modo/src/js/shared/_footer.js
@@ -1,25 +1,27 @@
-function adjustTextWidth() {
-  const container = document.querySelector(".case-studies__right");
-  const text = container.querySelector(".case-studies__text");
+document.addEventListener("DOMContentLoaded", function () {
+  function adjustTextWidth() {
+    const container = document.querySelector(".case-studies__right");
+    const text = container.querySelector(".case-studies__text");
 
-  container.style.width = "auto";
+    container.style.width = "auto";
 
-  const range = document.createRange();
-  range.selectNodeContents(text);
-  const rects = range.getClientRects();
+    const range = document.createRange();
+    range.selectNodeContents(text);
+    const rects = range.getClientRects();
 
-  let maxWidth = 0;
-  for (let i = 0; i < rects.length; i++) {
-    if (rects[i].width > maxWidth) maxWidth = rects[i].width;
+    let maxWidth = 0;
+    for (let i = 0; i < rects.length; i++) {
+      if (rects[i].width > maxWidth) maxWidth = rects[i].width;
+    }
+
+    container.style.width = `${Math.ceil(maxWidth)}px`;
   }
 
-  container.style.width = `${Math.ceil(maxWidth)}px`;
-}
+  window.addEventListener("load", () => {
+    requestAnimationFrame(adjustTextWidth);
+  });
 
-window.addEventListener("load", () => {
-  requestAnimationFrame(adjustTextWidth);
-});
-
-window.addEventListener("resize", () => {
-  adjustTextWidth();
+  window.addEventListener("resize", () => {
+    adjustTextWidth();
+  });
 });

f5215175e35cde43e7ab2bed9a1b9214a18aa303
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-06 14:58:35 +0000

    Collections overview styling

--- themes/modo/template-parts/home/collections.php
+++ themes/modo/template-parts/home/collections.php
@@ -1,2 +1,12 @@
 <section class="collections">
+	<div class="wrapper-padding">
+		<div class="wrapper">
+			<div class="collections__overview">
+				<h2 class="collections__title text-xs">Collections</h2>
+				<p class="collections__text text-2xl">From ancient artifacts to contemporary masterpieces, our collections
+					span centuries of human creativity and innovation. Each gallery invites you to explore history, culture, and
+					art through objects that have shaped the world.</p>
+			</div>
+		</div>
+	</div>
 </section>

891fa202e63b43e7bc1f9ebca382f88f0aa21f4b
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-06 00:43:36 +0000

    Carousel animation working

--- themes/modo/src/js/home/_exhibitions.js
+++ themes/modo/src/js/home/_exhibitions.js
@@ -1,20 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
   const items = document.querySelectorAll(".exhibitions__item");
   const images = document.querySelectorAll(".exhibitions__image");
+  const container = document.querySelector(".exhibitions__images");
+
+  let height = images[0].offsetHeight;
+  container.style.height = `${height}px`;
+
+  let lastActiveIndex = 0;
 
   items[0].classList.add("active");
   images[0].classList.add("active");
 
-  items.forEach((item, index) => {
+  items.forEach((item, itemIndex) => {
     item.addEventListener("mouseenter", () => {
       items.forEach((i) => {
         i.style.transitionDuration = "0.4s";
         i.classList.remove("active");
       });
-      images.forEach((img) => img.classList.remove("active"));
-
       item.classList.add("active");
-      images[index].classList.add("active");
+
+      const diff = itemIndex - lastActiveIndex;
+
+      images.forEach((image) => {
+        image.style.transform = `translateY(-${height * diff}px)`;
+      });
     });
   });
 });

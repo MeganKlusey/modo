dd5618e8a1eb52f2802258db94fafbe4ef576aa7
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-06 21:06:55 +0000

    Move images out of wrapper

--- themes/modo/template-parts/home/collections.php
+++ themes/modo/template-parts/home/collections.php
@@ -9,30 +9,30 @@
 						art through objects that have shaped the world.</p>
 				</div>
 			</div>
-			<ul class="collections__images">
-				<?php 
-					$args = [
-						'post_type' => 'collection',
-						'posts_per_page' => 4,
-						'order' => 'ASC',
-					];
-					$query = new WP_Query($args);
-					
-					if ( $query->have_posts() ) :
-					while ( $query->have_posts() ) : $query->the_post(); ?>
-				<li class="collections__item">
-					<img src="<?= get_the_post_thumbnail_url(); ?>" alt="" class="image collections__item-image" />
-					<div class="gradient"></div>
-					<div class="collections__item-info">
-						<p class="collections__item-title text-xs"><?php the_title(); ?></p>
-						<p class="collections__item-date text-xs">(2018)</p>
-					</div>
-				</li>
-				<?php
-					endwhile;
-					wp_reset_postdata();
-					endif; ?>
-			</ul>
 		</div>
+		<ul class="collections__images">
+			<?php 
+				$args = [
+					'post_type' => 'collection',
+					'posts_per_page' => 4,
+					'order' => 'ASC',
+				];
+				$query = new WP_Query($args);
+				
+				if ( $query->have_posts() ) :
+				while ( $query->have_posts() ) : $query->the_post(); ?>
+			<li class="collections__item">
+				<img src="<?= get_the_post_thumbnail_url(); ?>" alt="" class="image collections__item-image" />
+				<div class="gradient"></div>
+				<div class="collections__item-info">
+					<p class="collections__item-title text-xs"><?php the_title(); ?></p>
+					<p class="collections__item-date text-xs">(2018)</p>
+				</div>
+			</li>
+			<?php
+				endwhile;
+				wp_reset_postdata();
+				endif; ?>
+		</ul>
 	</div>
 </section>

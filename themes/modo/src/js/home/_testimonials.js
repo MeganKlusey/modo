759001c0b3d3168b6a74a907d02e4df1d0aeecdd
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-07 03:38:07 +0000

    Toggle active quote

--- themes/modo/src/js/home/_testimonials.js
+++ themes/modo/src/js/home/_testimonials.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const items = document.querySelectorAll(".testimonials__item");
   const images = document.querySelectorAll(".testimonials__image");
   const container = document.querySelector(".testimonials__images");
+  const info = document.querySelectorAll(".testimonials__info");
 
   let width = images[0].offsetWidth;
   container.style.width = `${width}px`;
@@ -10,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   items[0].classList.add("active");
   images[0].classList.add("active");
+  info[0].classList.add("active");
 
   items.forEach((item, itemIndex) => {
     item.addEventListener("click", () => {
@@ -25,6 +27,11 @@ document.addEventListener("DOMContentLoaded", function () {
       images.forEach((image) => {
         image.style.transform = `translateX(-${width * diff}px)`;
       });
+
+      info.forEach((i) => {
+        i.classList.remove("active");
+      });
+      info[itemIndex].classList.add("active");
     });
   });
 });

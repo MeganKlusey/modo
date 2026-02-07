a355965d9dfa12d29854a2bf4da7ec4a52532b74
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-07 01:58:12 +0000

    Testimonials layout

--- themes/modo/template-parts/home/testimonials.php
+++ themes/modo/template-parts/home/testimonials.php
@@ -25,7 +25,7 @@
 						if ( $query->have_posts() ) :
 						while ( $query->have_posts() ) : $query->the_post(); ?>
 					<q class="testimonials__quote text-2xl"><?= get_the_excerpt(); ?></q>
-					<p class="testimonials__author"><?= the_title(); ?></p>
+					<p class="testimonials__author text-sm"><?= the_title(); ?></p>
 					<?php
 						endwhile;
 						wp_reset_postdata();

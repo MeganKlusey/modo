8536430a39a16041bfecfe32c0e553845725859d
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-07 01:06:47 +0000

    Setup home testimonials template part

--- themes/modo/page-templates/home.php
+++ themes/modo/page-templates/home.php
@@ -8,6 +8,7 @@
 	<?php get_template_part('template-parts/home/about'); ?>
 	<?php get_template_part('template-parts/home/exhibitions'); ?>
 	<?php get_template_part('template-parts/home/collections'); ?>
+	<?php get_template_part('template-parts/home/testimonials'); ?>
 </main>
 
 <?php get_footer(); ?>

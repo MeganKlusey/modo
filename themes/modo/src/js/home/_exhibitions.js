7182a594ae02fe8d9ce0f461b896e44283d87f91
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-08 00:42:20 +0000

    Exhibitions slider responsive fix

--- themes/modo/src/js/home/_exhibitions.js
+++ themes/modo/src/js/home/_exhibitions.js
@@ -3,26 +3,40 @@ document.addEventListener("DOMContentLoaded", function () {
   const images = document.querySelectorAll(".exhibitions__image");
   const items = document.querySelectorAll(".exhibitions__item");
 
-  let height = images[0].offsetHeight;
-  let lastActiveIndex = 0;
-
-  container.style.height = `${height}px`;
+  let rafId = null;
+  let height = 0;
+  let activeIndex = 0;
+  let resizeTimeout;
 
   items[0].classList.add("active");
   images[0].classList.add("active");
 
-  items.forEach((item, itemIndex) => {
+  items.forEach((item, index) => {
     item.addEventListener("mouseenter", () => {
-      const diff = itemIndex - lastActiveIndex;
+      activeIndex = index;
 
-      items.forEach((i) => {
-        i.style.transitionProperty = "opacity";
-        i.style.transitionDuration = "0.4s";
-        i.classList.remove("active");
-      });
+      items.forEach((i) => i.classList.remove("active"));
       item.classList.add("active");
 
-      images.forEach((image) => (image.style.transform = `translateY(-${height * diff}px)`));
+      images.forEach((image) => (image.style.transform = `translateY(-${height * activeIndex}px)`));
     });
+  });
+
+  function updateHeight(noAnimation = false) {
+    height = images[0].offsetHeight;
+    container.style.height = `${height}px`;
+
+    images.forEach((image) => {
+      image.style.transitionDuration = noAnimation ? "0s" : "0.4s";
+      image.style.transform = `translateY(-${height * activeIndex}px)`;
+    });
+  }
+
+  window.addEventListener("load", updateHeight(false));
+
+  window.addEventListener("resize", () => {
+    updateHeight(true);
+    cancelAnimationFrame(rafId);
+    rafId = requestAnimationFrame(() => updateHeight(false));
   });
 });

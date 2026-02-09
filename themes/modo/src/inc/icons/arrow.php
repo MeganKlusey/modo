9e2b69f4d0e02de3117040a342daf7cb78e8dcc4
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-09 19:55:08 +0000

    Arrow icon refactor

--- themes/modo/src/inc/icons/arrow.php
+++ themes/modo/src/inc/icons/arrow.php
@@ -1,4 +1,4 @@
-<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none">
+<svg class="arrow" xmlns="http://www.w3.org/2000/svg">
 	<path d="M5.08316 4.55289H11.4471V10.9169M11.0052 4.99483L4.55283 11.4472" stroke-linecap="square"
-		stroke-miterlimit="10" stroke="#fff" stroke-width="1.5" />
+		stroke-miterlimit="10" />
 </svg>

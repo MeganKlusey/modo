d5240f149f1b5c28f499d3e4f68f3396719c2678
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-12 23:31:32 +0000

    Hero left GSAP

--- themes/modo/src/js/home/_hero.js
+++ themes/modo/src/js/home/_hero.js
@@ -4,18 +4,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let titleSplit = SplitText.create(".hero__title", { type: "words" });
 
-  tl.from(".hero__description", {
+  tl.from(".hero__offsite-links > li", {
     duration: 1,
-    x: -100,
+    x: 100,
     autoAlpha: 0,
-  }).from(
-    titleSplit.words,
-    {
-      duration: 1,
-      x: -100,
-      autoAlpha: 0,
-      stagger: -0.25,
-    },
-    0.25,
-  );
+    stagger: 0.25,
+  })
+    .from(
+      ".hero__description",
+      {
+        duration: 1,
+        x: -100,
+        autoAlpha: 0,
+      },
+      0.25,
+    )
+    .from(
+      titleSplit.words,
+      {
+        duration: 1,
+        x: -100,
+        autoAlpha: 0,
+        stagger: -0.25,
+      },
+      0.5,
+    );
 });

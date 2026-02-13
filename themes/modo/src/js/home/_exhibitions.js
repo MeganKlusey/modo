5711047ba8514342065bf7a20f56d69f4736c9fc
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-13 14:53:02 +0000

    Fix exhibitions height bug

--- themes/modo/src/js/home/_exhibitions.js
+++ themes/modo/src/js/home/_exhibitions.js
@@ -3,26 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
   const images = document.querySelectorAll(".exhibitions__image");
   const items = document.querySelectorAll(".exhibitions__item");
 
-  let rafId = null;
   let height = 0;
   let activeIndex = 0;
+
+  if (!images.length || !items.length) return;
 
   items[0].classList.add("active");
   images[0].classList.add("active");
 
-  items.forEach((item, index) => {
-    item.addEventListener("mouseenter", () => {
-      activeIndex = index;
-
-      items.forEach((i) => i.classList.remove("active"));
-      item.classList.add("active");
-
-      images.forEach((image) => (image.style.transform = `translateY(-${height * activeIndex}px)`));
-    });
-  });
-
   function updateHeight(noAnimation = false) {
     height = images[0].offsetHeight;
+
+    if (!height) return;
+
     container.style.height = `${height}px`;
 
     images.forEach((image) => {
@@ -31,11 +24,24 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  window.addEventListener("load", updateHeight(false));
+  items.forEach((item, index) => {
+    item.addEventListener("mouseenter", () => {
+      activeIndex = index;
+
+      items.forEach((i) => i.classList.remove("active"));
+      item.classList.add("active");
+
+      updateHeight();
+    });
+  });
+
+  const observer = new ResizeObserver(() => {
+    updateHeight(true);
+  });
+
+  observer.observe(images[0]);
 
   window.addEventListener("resize", () => {
     updateHeight(true);
-    cancelAnimationFrame(rafId);
-    rafId = requestAnimationFrame(() => updateHeight(false));
   });
 });

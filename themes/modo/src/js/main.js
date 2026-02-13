bbf72c4a67a814b5d42553552932b2065648abfa
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-13 21:11:20 +0000

    Create about js file

--- themes/modo/src/js/main.js
+++ themes/modo/src/js/main.js
@@ -2,6 +2,7 @@ import "../css/main.scss";
 import "./shared/_header";
 import "./shared/_footer";
 import "./home/_hero";
+import "./home/_about";
 import "./home/_exhibitions";
 import "./home/_collections";
 import "./home/_testimonials";
